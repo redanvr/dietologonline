6b161334a1a5118696e035f47bc70a834739f29c
Daniel <70541705+redanvr@users.noreply.github.com>
2022-03-27 19:54:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+const menuBurger = document.querySelector('.menu__burger');
+const menuBody = document.querySelector('.menu__body');
+if(menuBurger){
+    menuBurger.addEventListener("click", function() {
+        document.body.classList.toggle('_lock');
+        menuBurger.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
